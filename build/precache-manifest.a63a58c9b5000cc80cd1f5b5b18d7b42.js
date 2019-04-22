c9fc808790eb1a73b768ed67fb711440941ddf66
Josh Hall <joshua.v.hall@gmail.com>
2019-04-22 21:14:41 +1000

    ADDED: Production Build

---
+++ build/precache-manifest.a63a58c9b5000cc80cd1f5b5b18d7b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "419a214a3bc2e05ebe71",
+    "url": "/static/css/main.8d980800.chunk.css"
+  },
+  {
+    "revision": "419a214a3bc2e05ebe71",
+    "url": "/static/js/main.7fded8e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9df2a1515dd02f477bf",
+    "url": "/static/js/2.2eb1ee61.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39535ceaa5f2162489b32d1303b3831d",
+    "url": "/index.html"
+  }
+];
